fa565fa0ba97d96724647ffff33d9c1fc15b05b7
tuantu-wtpo <nguyentuantu19031997@gmail.com>
2022-10-16 11:26:18 +0700

    Show notify when create username

--- src/public/JS/createUsername.js
+++ src/public/JS/createUsername.js
@@ -1,14 +1,15 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const inputUsername = $('#userName');
-const btnSubmit = $('.btn-submit');
+const inputUsername = $("#userName");
+const btnSubmit = $(".btn-submit");
 
 btnSubmit.onclick = async (e) => {
   const userName = inputUsername.value;
   const id = btnSubmit.dataset.id;
-  const err = checkValidate('username', userName);
+  const err = checkValidate("username", userName);
   if (!err) {
+    animatePart.style.display = "block";
     const host = window.location.host;
     const url = `https://${host}/login/create-username`;
     const data = { userName, id };
@@ -16,7 +17,8 @@ btnSubmit.onclick = async (e) => {
     const result = res.data;
     const key = Object.keys(result)[0];
     const message = result[key];
-    if (key === 'error') return showNotify(key, message);
-    window.location.replace(res.request.responseURL);
+    const urlRedirect = result.url || res.request.responseURL;
+    animatePart.style.display = "none";
+    return showNotify(key, message, urlRedirect);
   }
 };
